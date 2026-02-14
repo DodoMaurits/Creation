71eed2afab4d5b42eb6aa5587f39e01d20d17961
DodoMaurits <m.kooge.j@gmail.com>
2026-02-14 23:21:49 +0100

    Update game.js

--- game.js
+++ game.js
@@ -1,33 +1,16 @@
-// Voorbeeld mappen en elementen
+// Voorbeeld mappen
 const mappen = [
-  {
-    naam: "Water",
-    icoon: "https://img.icons8.com/ios-filled/50/0000FF/water.png",
-    elementen: [
-      { naam: "H2O", icoon: "https://img.icons8.com/ios-filled/50/0000FF/water.png" },
-      { naam: "IJs", icoon: "https://img.icons8.com/ios-filled/50/ADD8E6/ice.png" }
-    ]
-  },
-  {
-    naam: "Vuur",
-    icoon: "https://img.icons8.com/ios-filled/50/FF0000/fire-element.png",
-    elementen: [
-      { naam: "Vuur", icoon: "https://img.icons8.com/ios-filled/50/FF0000/fire-element.png" }
-    ]
-  },
-  {
-    naam: "Aarde",
-    icoon: "https://img.icons8.com/ios-filled/50/8B4513/mountain.png",
-    elementen: [
-      { naam: "Klei", icoon: "https://img.icons8.com/ios-filled/50/DEB887/clay.png" }
+  { naam: "Heelal", icoon: "https://img.icons8.com/ios-filled/50/000000/galaxy.png", elementen: [{ naam: "Oerknal", icoon: "https://img.icons8.com/ios-filled/50/ffcc00/big-bang.png" }] },
+  { naam: "Krachten", icoon: "https://img.icons8.com/ios-filled/50/ff0000/atom.png", elementen: [
+      { naam: "Warmte", icoon: "https://img.icons8.com/ios-filled/50/ff6600/fire.png" },
+      { naam: "Kou", icoon: "https://img.icons8.com/ios-filled/50/00ccff/snowflake.png" }
     ]
   }
 ];
 
-// Combinaties
+// Voorbeeld combinaties
 const combinaties = [
-  { input: ["H2O", "Vuur"], output: { naam: "Stoom", icoon: "https://img.icons8.com/ios-filled/50/AAAAAA/steam.png", map: "Water" } },
-  { input: ["Klei", "H2O"], output: { naam: "Modder", icoon: "https://img.icons8.com/ios-filled/50/654321/mud.png", map: "Aarde" } }
+  { input: ["Oerknal", "Warmte"], output: { naam: "Energie", icoon: "https://img.icons8.com/ios-filled/50/ffcc00/energy.png", map: "Heelal" } }
 ];
 
 // Status
@@ -41,69 +24,49 @@ function init() {
   renderGroups();
 }
 
-// Render links en rechts de groepen
+// Render beide panelen groepen
 function renderGroups() {
   const leftDiv = document.getElementById("left-maps");
   const rightDiv = document.getElementById("right-maps");
+
   leftDiv.innerHTML = "";
   rightDiv.innerHTML = "";
 
   mappen.forEach((map, idx) => {
     // --- Links ---
-    const mapDiv = document.createElement("div");
-    mapDiv.className = "map";
-    mapDiv.innerHTML = `<img src="${map.icoon}" alt="${map.naam}"><span>${map.naam}</span>`;
-    if (leftOpenGroup === idx) mapDiv.classList.add("open");
-    mapDiv.addEventListener("click", () => toggleLeftGroup(idx, mapDiv));
-    leftDiv.appendChild(mapDiv);
+    const divL = document.createElement("div");
+    divL.className = "map";
+    divL.innerHTML = `<img src="${map.icoon}" alt="${map.naam}"><span>${map.naam}</span>`;
+    if (leftOpenGroup === idx) divL.classList.add("open");
+    divL.addEventListener("click", () => {
+      leftOpenGroup = (leftOpenGroup === idx) ? null : idx;
+      leftSelectedElement = null;
+      renderLeftElements();
+      renderGroups();
+    });
+    leftDiv.appendChild(divL);
 
     // --- Rechts ---
-    const mapDivR = document.createElement("div");
-    mapDivR.className = "map";
-    mapDivR.innerHTML = `<img src="${map.icoon}" alt="${map.naam}"><span>${map.naam}</span>`;
-    if (rightOpenGroup === idx) mapDivR.classList.add("open");
-    mapDivR.addEventListener("click", () => toggleRightGroup(idx, mapDivR));
-    rightDiv.appendChild(mapDivR);
+    const divR = document.createElement("div");
+    divR.className = "map";
+    divR.innerHTML = `<img src="${map.icoon}" alt="${map.naam}"><span>${map.naam}</span>`;
+    if (rightOpenGroup === idx) divR.classList.add("open");
+    divR.addEventListener("click", () => {
+      rightOpenGroup = (rightOpenGroup === idx) ? null : idx;
+      rightSelectedElement = null;
+      renderRightElements();
+      renderGroups();
+    });
+    rightDiv.appendChild(divR);
   });
-}
-
-// Toggle linker groep open/close
-function toggleLeftGroup(idx, div) {
-  if (leftOpenGroup === idx) {
-    leftOpenGroup = null;
-    leftSelectedElement = null;
-  } else {
-    leftOpenGroup = idx;
-    leftSelectedElement = null;
-  }
-  renderGroups();
-  renderLeftElements();
-}
-
-// Toggle rechter groep open/close
-function toggleRightGroup(idx, div) {
-  if (rightOpenGroup === idx) {
-    rightOpenGroup = null;
-    rightSelectedElement = null;
-  } else {
-    rightOpenGroup = idx;
-    rightSelectedElement = null;
-  }
-  renderGroups();
-  renderRightElements();
 }
 
 // Render linker elementen
 function renderLeftElements() {
-  const leftDiv = document.getElementById("left-maps");
-  // Verwijder oude container
-  const old = leftDiv.querySelector(".element-container");
-  if (old) old.remove();
-
+  const container = document.getElementById("left-elements-container");
+  container.innerHTML = "";
   if (leftOpenGroup === null) return;
 
-  const container = document.createElement("div");
-  container.className = "element-container";
   mappen[leftOpenGroup].elementen.forEach(el => {
     const div = document.createElement("div");
     div.className = "element";
@@ -116,19 +79,14 @@ function renderLeftElements() {
     });
     container.appendChild(div);
   });
-  leftDiv.appendChild(container);
 }
 
 // Render rechter elementen
 function renderRightElements() {
-  const rightDiv = document.getElementById("right-maps");
-  const old = rightDiv.querySelector(".element-container");
-  if (old) old.remove();
-
+  const container = document.getElementById("right-elements-container");
+  container.innerHTML = "";
   if (rightOpenGroup === null) return;
 
-  const container = document.createElement("div");
-  container.className = "element-container";
   mappen[rightOpenGroup].elementen.forEach(el => {
     const div = document.createElement("div");
     div.className = "element";
@@ -141,23 +99,21 @@ function renderRightElements() {
     });
     container.appendChild(div);
   });
-  rightDiv.appendChild(container);
 }
 
-// Controleer combinatie
+// Controleer combinaties
 function tryCombine() {
   if (!leftSelectedElement || !rightSelectedElement) return;
 
-  const combinatie = combinaties.find(c =>
-    (c.input[0] === leftSelectedElement && c.input[1] === rightSelectedElement) ||
-    (c.input[1] === leftSelectedElement && c.input[0] === rightSelectedElement)
+  const combi = combinaties.find(c =>
+    (c.input.includes(leftSelectedElement) && c.input.includes(rightSelectedElement))
   );
 
-  if (combinatie) {
-    alert(`Nieuw element ontdekt: ${combinatie.output.naam}`);
-    const map = mappen.find(m => m.naam === combinatie.output.map);
-    if (!map.elementen.some(e => e.naam === combinatie.output.naam)) {
-      map.elementen.push({ naam: combinatie.output.naam, icoon: combinatie.output.icoon });
+  if (combi) {
+    alert(`Nieuw element ontdekt: ${combi.output.naam}`);
+    const map = mappen.find(m => m.naam === combi.output.map);
+    if (!map.elementen.some(e => e.naam === combi.output.naam)) {
+      map.elementen.push({ naam: combi.output.naam, icoon: combi.output.icoon });
     }
 
     // reset alles
@@ -167,8 +123,9 @@ function tryCombine() {
     rightSelectedElement = null;
 
     renderGroups();
+    renderLeftElements();
+    renderRightElements();
   }
 }
 
-// Start
 init();
