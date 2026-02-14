ce8e5f73ab8c79562689393c6c42ba21ac640c56
DodoMaurits <m.kooge.j@gmail.com>
2026-02-14 23:01:38 +0100

    Update game.js

--- game.js
+++ game.js
@@ -1,4 +1,37 @@
+// voorbeeld mappen en elementen
+const mappen = [
+  {
+    naam: "Water",
+    icoon: "https://img.icons8.com/ios-filled/50/0000FF/water.png",
+    elementen: [
+      { naam: "H2O", icoon: "https://img.icons8.com/ios-filled/50/0000FF/water.png" },
+      { naam: "IJs", icoon: "https://img.icons8.com/ios-filled/50/ADD8E6/ice.png" }
+    ]
+  },
+  {
+    naam: "Vuur",
+    icoon: "https://img.icons8.com/ios-filled/50/FF0000/fire-element.png",
+    elementen: [
+      { naam: "Vuur", icoon: "https://img.icons8.com/ios-filled/50/FF0000/fire-element.png" }
+    ]
+  },
+  {
+    naam: "Aarde",
+    icoon: "https://img.icons8.com/ios-filled/50/8B4513/mountain.png",
+    elementen: [
+      { naam: "Klei", icoon: "https://img.icons8.com/ios-filled/50/DEB887/clay.png" }
+    ]
+  }
+];
+
+// voorbeeld combinaties
+const combinaties = [
+  { input: ["H2O", "Vuur"], output: { naam: "Stoom", icoon: "https://img.icons8.com/ios-filled/50/AAAAAA/steam.png", map: "Water" } },
+  { input: ["Klei", "Water"], output: { naam: "Modder", icoon: "https://img.icons8.com/ios-filled/50/654321/mud.png", map: "Aarde" } }
+];
+
 let selectedLeft = null;
+let leftElementSelected = null;
 
 function init() {
   renderMaps();
@@ -16,9 +49,10 @@ function renderMaps() {
     div.dataset.index = index;
     div.innerHTML = `<img src="${map.icoon}" alt="${map.naam}"><span>${map.naam}</span>`;
     div.addEventListener("click", () => toggleMap(index));
-    
+
     if (selectedLeft === index) {
-      document.getElementById("left-maps").appendChild(div);
+      div.classList.add("selected");
+      leftMapsDiv.appendChild(div);
     } else {
       rightMapsDiv.appendChild(div);
     }
@@ -33,6 +67,7 @@ function toggleMap(index) {
   } else {
     selectedLeft = index;
   }
+  leftElementSelected = null;
   renderMaps();
 }
 
@@ -42,6 +77,7 @@ function renderElements() {
   leftElementsDiv.innerHTML = "";
   rightElementsDiv.innerHTML = "";
 
+  // elementen van geselecteerde groep
   if (selectedLeft !== null) {
     mappen[selectedLeft].elementen.forEach(el => {
       const div = document.createElement("div");
@@ -54,6 +90,7 @@ function renderElements() {
     });
   }
 
+  // elementen van andere groepen
   mappen.forEach((map, idx) => {
     if (idx !== selectedLeft) {
       map.elementen.forEach(el => {
@@ -69,10 +106,7 @@ function renderElements() {
   });
 }
 
-let leftElementSelected = null;
-
 function selectLeftElement(div, naam) {
-  // deselecteer alle
   document.querySelectorAll("#left-elements .element").forEach(el => el.classList.remove("selected"));
   div.classList.add("selected");
   leftElementSelected = naam;
@@ -81,14 +115,13 @@ function selectLeftElement(div, naam) {
 function combineWithLeft(rightNaam, rightMapIdx) {
   if (!leftElementSelected) return;
 
-  const combinatie = combinaties.find(c => 
+  const combinatie = combinaties.find(c =>
     (c.input[0] === leftElementSelected && c.input[1] === rightNaam) ||
     (c.input[1] === leftElementSelected && c.input[0] === rightNaam)
   );
 
   if (combinatie) {
     alert(`Nieuw element ontdekt: ${combinatie.output.naam}`);
-    // voeg toe aan de juiste map
     const map = mappen.find(m => m.naam === combinatie.output.map);
     if (!map.elementen.some(e => e.naam === combinatie.output.naam)) {
       map.elementen.push({ naam: combinatie.output.naam, icoon: combinatie.output.icoon });
