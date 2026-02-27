196c3edf79d254cb55f8591ca6b5b05cc8586a71
DodoMaurits <m.kooge.j@gmail.com>
2026-02-27 11:30:38 +0100

    Update game.js

--- game.js
+++ game.js
@@ -2163,7 +2163,7 @@ function updateTimelineLabel() {
 
   const percentage = (maxTime - currentTime) / maxTime;
   
-  const offset = 20; // schuif label naar rechts, pas aan naar wens
+  const offset = 18; // schuif label naar rechts, pas aan naar wens
   const labelPos = timelineRect.left + percentage * timelineWidth + offset;
   
   timelineLabel.style.left = labelPos + "px";
