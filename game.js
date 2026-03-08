804b17198173affb78a38dcbf8a60d56b29295a0
DodoMaurits <m.kooge.j@gmail.com>
2026-03-08 23:13:37 +0100

    Update game.js

--- game.js
+++ game.js
@@ -626,7 +626,7 @@ const combinaties = [
     ],
     output: [
       { naam: "Wind", icoon: "icons/Wind.png", map: "Lucht", 
-        quote: "Sunshine is delicious, rain is refreshing, wind braces us up, sonw is exhilarating: there is really no such thing as bad weather, only different kinds of good weather <br><br>- John Ruskin"
+        quote: "Sunshine is delicious, rain is refreshing, wind braces us up, snow is exhilarating: there is really no such thing as bad weather, only different kinds of good weather <br><br>- John Ruskin"
       }
     ]
   },
