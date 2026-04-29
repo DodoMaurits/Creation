7547f472868c8fc0527b281399f028ec7b25b965
DodoMaurits <m.kooge.j@gmail.com>
2026-04-29 22:50:23 +0200

    Update gametest.js

--- gametest.js
+++ gametest.js
@@ -213,7 +213,6 @@ const mappen = [
       { naam: "Oog", icoon: "icons/Oog.png" },
       { naam: "Bloed", icoon: "icons/Bloed.png" },
       { naam: "Parel", icoon: "icons/Parel.png" },
-      { naam: "Zijde", icoon: "icons/Zijde.png" },
       { naam: "Vleugels", icoon: "icons/Vleugels.png" },
       { naam: "Blad", icoon: "icons/Blad.png" },
       { naam: "Zaadjes", icoon: "icons/Zaadjes.png" },
@@ -5929,7 +5928,7 @@ const combinaties = [
     input: ["Spinnen", "Glucose"],
     hint: `Geef de spin wat ze nodig heeft en ze maakt er iets moois van.`,
     output: [
-      { naam: "Zijde", icoon: "icons/Zijde.png", map: "Biologie", 
+      { naam: "Zijde", icoon: "icons/Zijde.png", map: "Materialen", 
         quote: `Silk does for the body what diamonds do for the hand
         <br><br>- Oscar de la Renta`
       }
