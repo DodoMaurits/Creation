5e763f833629f8c1b8cb32b8455d768ace5e6de5
DodoMaurits <m.kooge.j@gmail.com>
2026-05-19 00:28:15 +0200

    Update gametest.js

--- gametest.js
+++ gametest.js
@@ -14223,7 +14223,7 @@ const combinaties = [
     hint: `Een kampvuur is heter als je er een stenen huisje van maakt..`,
     tijd: 31000,
     output: [
-      { naam: "Oven", icoon: "icons/Oven.png", map: "Uitvindingen", 
+      { naam: "Oven", icoon: "icons/Oven.png", map: "Vuur", 
         quote: `Control of fire reached a new level with the enclosed oven, where heat itself became a tool
         <br><br>- Lewis Mumford`
       }
