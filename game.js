9598024c4031d88691c704d8f3eda8312b074603
DodoMaurits <m.kooge.j@gmail.com>
2026-06-01 10:22:00 +0200

    Add Amaryllis to output options in game.js

--- game.js
+++ game.js
@@ -10475,6 +10475,10 @@ const combinaties = [
     hint: `Sommige monocotylen voelen zich thuis op de savanne.`,
     tijd: 50_000_000,
     output: [
+      { naam: "Amaryllissen", icoon: "icons/Amaryllissen.png", map: "Bloemen", 
+        quote: `An amaryllis has the confidence to bloom magnificently when all else is sleeping
+        <br><br>- Beverley Nichols`
+      },
       { naam: "Gladiolen", icoon: "icons/Gladiolen.png", map: "Bloemen", 
         quote: `The tall, sword-like form of the gladiolus gave rise to its symbolic association with battle and loss.
         <br><br>- Christopher Brickell`
@@ -13543,6 +13547,10 @@ const combinaties = [
     hint: `Sommige monocotylen voelen zich thuis in het fynbos.`,
     tijd: 20_000_000,
     output: [
+      { naam: "Amaryllissen", icoon: "icons/Amaryllissen.png", map: "Bloemen", 
+        quote: `An amaryllis has the confidence to bloom magnificently when all else is sleeping
+        <br><br>- Beverley Nichols`
+      },
       { naam: "Gladiolen", icoon: "icons/Gladiolen.png", map: "Bloemen", 
         quote: `The tall, sword-like form of the gladiolus gave rise to its symbolic association with battle and loss.
         <br><br>- Christopher Brickell`
